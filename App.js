bc748a82f62fa17ede95bb1ad8bcb4660d54ad38
Saniya452 <ahmedsania198@gmail.com>
2025-03-04 21:54:53 +0500

    Add New Features

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import LoginScreen from './app/src/Screens/login';
-import SignupScreen from './app/src/Screens/signup';
-import InfoScreen from './app/src/Screens/info';
-import Mcqs from './app/src/Screens/mcqs';
-import Questions from './app/src/Screens/questions';
-import onboarding from './app/src/Screens/onboarding';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./app/src/Screens/login";
+import SignupScreen from "./app/src/Screens/signup";
+import InfoScreen from "./app/src/Screens/info";
+import Mcqs from "./app/src/Screens/mcqs";
+import Questions from "./app/src/Screens/questions";
+import Onboarding from "./app/src/Screens/onboarding";
 
 const Stack = createStackNavigator();
 
@@ -16,34 +16,14 @@ const App = () => {
       <Stack.Navigator initialRouteName="Onboarding">
         <Stack.Screen
           name="Onboarding"
-          component={onboarding}
+          component={Onboarding}
           options={{ headerShown: false }}
         />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Info"
-          component={InfoScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Mcqs"
-          component={Mcqs}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Questions"
-          component={Questions}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Info" component={InfoScreen} />
+        <Stack.Screen name="Mcqs" component={Mcqs} />
+        <Stack.Screen name="Questions" component={Questions} />
       </Stack.Navigator>
     </NavigationContainer>
   );
